8b4a521c2927bb946d3b7a9937c57abd791ff71c
Aakash Gupta <guptaakash9236@gmail.com>
2026-07-30 00:44:05 +0530

    import and make moduler files

---
+++ Nodejs/Day2/math.js
@@ -0,0 +1,37 @@
+/*const sum = require("./add_sub")
+ console.log("Sum of two number is ",sum(10,20))
+
+*/
+
+
+
+/* const { addFn, subFn } = require('./add_sub')
+console.log("sum of two number is ", addFn(10, 20));
+console.log("subtraction of two number is ", subFn(30, 20));
+
+*/
+
+/*const math = require('./add_sub')
+console.log("sum of two number is ", math.add(10, 20));
+console.log("subtraction of two number is ", math.sub(30, 20));
+
+*/
+
+
+
+
+
+
+
+/*const math = require('./add_sub')
+
+console.log("sum is ", math.add(10, 20))
+console.log("sub is ", math.sub(10, 20))
+
+*/
+
+
+const { add, sub } = require('./add_sub')
+
+console.log("sum is ", add(10, 20))
+console.log("sub is ", sub(10, 20))
