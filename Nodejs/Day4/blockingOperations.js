e1c766a43199a329588bfafa733f2af0041115da
Aakash Gupta <guptaakash9236@gmail.com>
2026-08-02 01:38:51 +0530

    cpu length finding

--- Nodejs/Day4/blockingOperations.js
+++ Nodejs/Day4/blockingOperations.js
@@ -1,9 +1,12 @@
 const { nonBlocking } = require("./nonBlockingOperations.js");
 const fs = require('fs')
+const os = require('os')
 
 fs.writeFileSync("./test.txt", "Hello I am Aakash")
 
 const result = fs.readFileSync("./test.txt", "utf-8")
 console.log(result)
+
+console.log(os.cpus().length)
 
 nonBlocking()
